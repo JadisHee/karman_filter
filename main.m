9d388087e7487db8b683e8158ac6fefcea198d61
JadisHee <96834552+JadisHee@users.noreply.github.com>
2023-07-20 17:46:52 +0800

    add main
    matlab实例

--- main.m
+++ main.m
@@ -1,67 +1,71 @@
+%% 通过imu预测的速度与里程计计算所得位移进行一维卡尔曼融合
+% 其中imu预测的速度作为控制量输入，用于预测先验位移
+% 里程计计算所得位移作为观测量，用于校正出后验估计值
+
 clear;
 close all;
 clc;
-data = xlsread('data_0328_2');
 
-% 读取数据
-load('acc_.mat');
-load('z.mat')
+% 读取采集的数据文档
+data = xlsread('./data/record_2.csv');
 
-u = acc_(1,:);
-t = (data(1:301,1))'/1000;
+% 采样间隔，单位s
+dt = 0.016;
 
-z = data(1:301,11)/1000000;
+% imu预测的速度
+velocity_imu = data(:,3);
 
-% x = data(1:301,11)/1000000;
-
-v_var = 1;
-
-% z = zeros(1,length(t));
-% for i = 1:1:length(z)
-%     
-%     v_noise = 0 + v_var*randn();
-%     z(i) = x(i) + v_noise;
-% end
-
-X = [0;0];
-
-P = [0.0001,0;0,0.0001];
-Q = [0.001,0;0,0.001];
-H = [1,0];
-R = 1;
-
-X_record = zeros(1,length(t));
-
-delta_t = zeros(1,length(t));
-for i = 1:1:length(delta_t)
-    if i == 1
-        delta_t = 0;
-    else
-        delta_t = t(i) - t(i-1);
-    end
-    A = [1,delta_t;0,1];
-    B = [0.5 * delta_t^2;delta_t];
-    
-    X_predict = A * X + u(i).*B;
-    P_predict = A * P * A' + Q;
-    
-    K = P_predict * H' / (H * P_predict * H' + R);
-    X = X_predict + K * (z(i) - H * X_predict);
-    P = (eye(2) - K*H) * P_predict;
-    X_record(i) = X(1,1);
-    
+% 里程计速度
+velocity_odom = data(:,2);
+% 定义观测量，并把通过里程计速度计算出观测位移
+Z = zeros(length(velocity_odom(:)),1);
+for i = 2:1:length(Z)
+    Z(i) = Z(i-1) + velocity_odom(i-1) * dt;
 end
 
+% 控制量
+u = velocity_imu;
+% 定义并初始化状态变量（预测量）
+X = 0;
+% 定义状态矩阵
+A = 1;
+% 定义控制矩阵
+B = dt;
+% 定义预测过程噪声方差
+Q = 0.001;
 
+% 定义观测量与状态变量的关系矩阵
+H = 1;
+% 测量噪声方差
+R = 1;
 
+% 定义与初始化协方差矩阵
+P = 1;
 
+% 定义预测量向量，用于储存每次预测后的值
+X_hat = zeros(length(velocity_odom(:)),1);
+% 迭代
+for i = 1:1:length(Z)
+    % 先验估计
+    X_predict = A * X + B * u(i);
+    % 先验估计协方差矩阵
+    P_predict = A * P * A' + Q;
+    
+    % 校正过程
+    % 卡尔曼增益
+    K = P_predict * H' / (H * P_predict * H' + R);
+    % 后验估计
+    X = X_predict + K * (Z(i) - H * X_predict);
+    % 更新协方差矩阵
+    P = (1 - K * H) * P_predict;
+    
+    % 赋值
+    X_hat(i) = X;
+end
 
-
-
-figure()
-
-plot(t,z)
-hold on 
-plot(t,X_record)
-legend('测量位移','预测位移')
-
+%% 绘图
+figure(1)
+plot(Z)
+hold on
+plot(X_hat)
+legend('轮式里程计测量值','卡尔曼估计值')
